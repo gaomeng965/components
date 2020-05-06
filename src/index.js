5d5835b16929c69e39a9cc990c4212f87987875a
gaomeng <menggao965@163.com>
2020-05-06 11:09:08 +0800

    fix: change catalog

---
+++ src/index.js
@@ -0,0 +1,18 @@
+/*
+ * @Author: your name
+ * @Date: 2020-05-06 11:05:12
+ * @LastEditTime: 2020-05-06 11:05:36
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /ncp-design/src/index.js
+ */
+/*
+ * @Author: gaomeng
+ * @Date: 2020-04-29 15:50:40
+ * @LastEditTime: 2020-04-30 13:26:34
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /组件库模块/ncp-design/src/components/index.ts
+ */
+
+export { default as Button } from './components/Button';
